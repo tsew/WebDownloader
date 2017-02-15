7cb1d3140c6687256255c9ab95640553cec8a8ba
Al West <al.west@valtech.co.uk>
2017-02-15 20:07:14 +0000

    Add project files.

---
+++ WindowsFormsApplication1/WebDownloader.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WebDownloader
+{
+    partial class WebDownloaderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DownloadProgressBar = new System.Windows.Forms.ProgressBar();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.URLTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DownloadProgressBar
+            // 
+            this.DownloadProgressBar.Location = new System.Drawing.Point(12, 99);
+            this.DownloadProgressBar.Name = "DownloadProgressBar";
+            this.DownloadProgressBar.Size = new System.Drawing.Size(260, 23);
+            this.DownloadProgressBar.TabIndex = 0;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(12, 39);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(75, 23);
+            this.DownloadButton.TabIndex = 1;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(12, 74);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(37, 13);
+            this.StatusLabel.TabIndex = 2;
+            this.StatusLabel.Text = "Status";
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(197, 39);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 3;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // URLTextBox
+            // 
+            this.URLTextBox.Location = new System.Drawing.Point(12, 13);
+            this.URLTextBox.Name = "URLTextBox";
+            this.URLTextBox.Size = new System.Drawing.Size(260, 20);
+            this.URLTextBox.TabIndex = 4;
+            this.URLTextBox.Text = "https://dl.armbian.com/bananapi/Ubuntu_xenial_next.7z";
+            this.URLTextBox.TextChanged += new System.EventHandler(this.URLTextBox_TextChanged);
+            // 
+            // WebDownloaderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 138);
+            this.Controls.Add(this.URLTextBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.DownloadProgressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "WebDownloaderForm";
+            this.Text = "Web Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar DownloadProgressBar;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox URLTextBox;
+    }
+}
+
---
+++ WindowsFormsApplication1/WebDownloader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Net;
+
+namespace WebDownloader
+{
+    public partial class WebDownloaderForm : Form
+    {
+        public WebDownloaderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            StartDownload();
+        }
+
+        bool isDownloading = false, validURL = true;
+        WebClient client;
+
+        private void StartDownload()
+        {
+            if (isDownloading)
+            {
+                MessageBox.Show("Already downloading");
+                return;
+            }
+            if (!validURL)
+            {
+                MessageBox.Show("URL is not valid.\n Did you specify \"http://\" or \"https://\"");
+                return;
+            }
+            isDownloading = true;
+            client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            client.DownloadFileAsync(new Uri(URLTextBox.Text), "armbian.7z");
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+            StatusLabel.Text = "Downloaded " + (e.BytesReceived / 1024).ToString() + " / "
+                + (e.TotalBytesToReceive / 1024).ToString() + " "
+                + ((int)percentage).ToString() + "%";
+            DownloadProgressBar.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+                StatusLabel.Text = "Cancelled";
+            else if (e.Error != null)
+                StatusLabel.Text = e.Error.Message;
+            else
+                StatusLabel.Text = "Completed";
+            isDownloading = false;
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            if (isDownloading)
+            {
+                client.CancelAsync();
+                StatusLabel.Text = "Stopped";
+            }
+        }
+
+        private void URLTextBox_TextChanged(object sender, EventArgs e)
+        {
+            Uri uriResult;
+            validURL = Uri.TryCreate(URLTextBox.Text, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (validURL)
+                StatusLabel.Text = "Valid URL";
+            else
+                StatusLabel.Text = "Not a valid URL";
+        }
+    }
+}
+
